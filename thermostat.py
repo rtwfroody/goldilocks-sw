f5f2fd45dbbd1ba47104cd0556685f6d70710791
Tim Newsome <tim@sifive.com>
2022-09-16 19:49:52 -0700

    Print out touchscreen data.

--- thermostat.py
+++ thermostat.py
@@ -11,6 +11,8 @@ import traceback
 import adafruit_ntp
 import time
 import adafruit_pcf8523
+from adafruit_stmpe610 import Adafruit_STMPE610_SPI
+import digitalio
 
 class Mqtt(object):
     mqtt_prefix = "goldilocks/sensor/temperature_F/"
@@ -59,14 +61,11 @@ class Mqtt(object):
         return list(self.temperatures.items())
 
 class Display(object):
-    def __init__(self):
+    def __init__(self, spi):
         displayio.release_displays()
-        spi = board.SPI()
-        tft_cs = board.D9
-        tft_dc = board.D10
-
         display_bus = displayio.FourWire(
-            spi, command=tft_dc, chip_select=tft_cs, reset=board.D6
+            spi,
+            command=board.D10, chip_select=board.D9
         )
         self.display = adafruit_ili9341.ILI9341(display_bus, width=320, height=240)
 
@@ -83,34 +82,41 @@ class Display(object):
         self.display.show(splash)
 
     def show_table(self, now, table):
-        text_group = displayio.Group(x=10, y=10)
-        # This does a fucking network call...
         lines = ["%04d-%02d-%02d %02d:%02d:%02d" % (
             now.tm_year, now.tm_mon, now.tm_mday,
             now.tm_hour, now.tm_min, now.tm_sec
         )]
         lines += table.items()
         text = "\n".join(str(i) for i in lines)
-        text_area = label.Label(terminalio.FONT, text=text, color=0xFFFF00)
-        text_group.append(text_area)  # Subgroup for text scaling
-        self.display.show(text_group)
+        text_area = label.Label(terminalio.FONT, text=text, color=0xFFFF00, x=10, y=10)
+        self.display.show(text_area)
 
 class Thermostat(object):
     def __init__(self):
-        self.display = Display()
+        ### Hardware devices
+        # Get splash screen going first.
+        spi = board.SPI()
+        self.display = Display(spi)
+        i2c = board.I2C()
+        self.rtc = adafruit_pcf8523.PCF8523(i2c)
+
+        # Start network, and use it.
         wifi.radio.connect(secrets.ssid, secrets.password)
         self.socket_pool = socketpool.SocketPool(wifi.radio)
         self.mqtt = Mqtt(secrets.mqtt_server, secrets.mqtt_port,
                          secrets.mqtt_username, secrets.mqtt_password,
                          self.socket_pool)
-        self.temperatures = {}
-        self.i2c = board.I2C()
-        self.rtc = adafruit_pcf8523.PCF8523(self.i2c)
 
         # TODO: How do you deal with timezones?
         ntp = adafruit_ntp.NTP(self.socket_pool, tz_offset=-7)
         # Sync at boot up
         self.rtc.datetime = ntp.datetime
+
+        touch_cs = digitalio.DigitalInOut(board.D6)
+        self.touch = Adafruit_STMPE610_SPI(spi, touch_cs)
+        
+        ### Local variables.
+        self.temperatures = {}
 
     def now(self):
         return self.rtc.datetime
@@ -120,8 +126,10 @@ class Thermostat(object):
             temperature_updates = self.mqtt.poll()
             for (k, v) in temperature_updates:
                 self.temperatures[k] = v
-            if self.temperatures:
-                self.display.show_table(self.now(), self.temperatures)
+            self.display.show_table(self.now(), self.temperatures)
+
+            while not self.touch.buffer_empty:
+                print(self.touch.read_data())
 
 def main():
     thermostat = Thermostat()
