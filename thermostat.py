777fe47a2e4e73c76b074aa9aaf2f40fbabd1766
Tim Newsome <tim@sifive.com>
2022-09-18 14:14:05 -0700

    Basic button highlighting in response to touch.

--- thermostat.py
+++ thermostat.py
@@ -61,8 +61,61 @@ class Mqtt(object):
             # We'll connect again the next poll()
         return list(self.temperatures.items())
 
-class Display(object):
+class TouchScreenEvent(object):
+    DOWN = 0
+    UP = 1
+    DRAG = 2
+    def __init__(self, typ, x, y):
+        self.typ = typ
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return "TouchScreenEvent(%d, %d, %d)" % (self.typ, self.x, self.y)
+
+class TouchScreenEvents(object):
+    def __init__(self, touch):
+        self.touch = touch
+        self.last = None
+
+    def poll(self):
+        point = self.touch.touch_point
+        last = self.last
+        self.last = point
+        if point:
+            x, y, pressure = point
+            if last:
+                return TouchScreenEvent(TouchScreenEvent.DRAG, x, y)
+            else:
+                return TouchScreenEvent(TouchScreenEvent.DOWN, x, y)
+        else:
+            if last:
+                return TouchScreenEvent(TouchScreenEvent.UP, last[0], last[1])
+
+class Gui(object):
     # TODO: play with backlight brightness, https://learn.adafruit.com/making-a-pyportal-user-interface-displayio/display
+    def __init__(self, spi):
+        self.width = 320
+        self.height = 240
+
+        # Set up display
+        displayio.release_displays()
+        display_bus = displayio.FourWire(spi, command=board.D10, chip_select=board.D9)
+        self.display = adafruit_ili9341.ILI9341(display_bus, width=self.width, height=self.height)
+
+        splash = self.make_splash()
+        self.display.show(splash)
+
+        # Set up touchscreen
+        touch_cs = digitalio.DigitalInOut(board.D6)
+        touch = Adafruit_STMPE610_SPI(spi, touch_cs,
+                calibration=((276, 3820), (378, 3743)))
+        self.tse = TouchScreenEvents(touch)
+
+        self.make_main()
+
+        self.selected = None
+
     def make_splash(self):
         # Make the display context
         splash = displayio.Group()
@@ -76,50 +129,38 @@ class Display(object):
         return splash
 
     def make_main(self):
-        main = displayio.Group()
+        self.main_group = displayio.Group()
         spacing = 10
         button_height = 40
-        preset_group = displayio.Group(
-            x=int(spacing/2),
-            y=int(self.height - button_height - spacing/2))
+        group_x = int(spacing/2)
+        group_y = int(self.height - button_height - spacing/2)
         presets = ["Sleep", "Away", "Home"]
-        buttons = []
+        self.main_buttons = []
         for i, preset in enumerate(presets):
             button = Button(
-                x=int(i * self.width / 3 + spacing / 2),
-                y=0,
+                x=group_x + int(i * self.width / 3 + spacing / 2),
+                y=group_y,
                 width=int(self.width / 3 - spacing),
                 height = button_height,
                 label=preset,
-                label_font=terminalio.FONT)
-            buttons.append(button)
-            preset_group.append(button)
-        main.append(preset_group)
+                label_font=terminalio.FONT,
+                style=Button.ROUNDRECT)
+            self.main_buttons.append(button)
+            self.main_group.append(button)
 
         info_group = displayio.Group(x=int(spacing/2), y=80)
         self.time_label = label.Label(terminalio.FONT, text="time", color=0xFFFFFF, x=10, y=10)
         self.temperature_label = label.Label(terminalio.FONT, text="temperature", color=0xFF80FF, x=10, y=30)
         info_group.append(self.time_label)
         info_group.append(self.temperature_label)
-        main.append(info_group)
+        self.main_group.append(info_group)
 
-        return main
-
-    def __init__(self, spi):
-        self.width = 320
-        self.height = 240
-
-        displayio.release_displays()
-        display_bus = displayio.FourWire(
-            spi,
-            command=board.D10, chip_select=board.D9
-        )
-        self.display = adafruit_ili9341.ILI9341(display_bus, width=self.width, height=self.height)
-
-        splash = self.make_splash()
-        self.display.show(splash)
-
-        self.main_group = self.make_main()
+        #self.bitmap = displayio.Bitmap(self.display.width, self.display.height, 2)
+        #palette = displayio.Palette(2)
+        #palette[0] = 0x000000
+        #palette[1] = 0x8fffff
+        #tile_grid = displayio.TileGrid(self.bitmap, pixel_shader=palette)
+        #self.main_group.append(tile_grid)
 
     def update_time(self, t):
         self.time_label.text = "%04d-%02d-%02d %02d:%02d:%02d" % (
@@ -130,15 +171,41 @@ class Display(object):
     def update_temperatures(self, temps):
         self.temperature_label.text = "\n".join(repr(i) for i in temps.items())
 
-    def main(self):
+    #def draw_pixel(self, x, y):
+    #    self.bitmap[int(x), int(y)] = 1
+
+    def show_main(self):
         self.display.show(self.main_group)
+
+    def poll(self):
+        event = self.tse.poll()
+        if event:
+            print(event)
+            x = int(self.width * event.x / 4096)
+            y = int(self.height * event.y / 4096)
+            if self.selected:
+                if self.selected.contains((x, y)):
+                    # No change needed
+                    return
+                else:
+                    self.selected.selected = False
+                    self.selected = None
+
+            for button in self.main_buttons:
+                if button.contains((x, y)):
+                    button.selected = True
+                    self.selected = button
+        else:
+            if self.selected:
+                self.selected.selected = False
+                self.selected = None
 
 class Thermostat(object):
     def __init__(self):
         ### Hardware devices
         # Get splash screen going first.
         spi = board.SPI()
-        self.display = Display(spi)
+        self.gui = Gui(spi)
         i2c = board.I2C()
         self.rtc = adafruit_pcf8523.PCF8523(i2c)
 
@@ -157,13 +224,13 @@ class Thermostat(object):
         except OSError as e:
             # Doesn't always work.
             print("NTP failed: %r" % e)
-
-        touch_cs = digitalio.DigitalInOut(board.D6)
-        self.touch = Adafruit_STMPE610_SPI(spi, touch_cs)
         
         ### Local variables.
         self.temperatures = {}
         self.last_stamp = 0
+
+        self.min_point = [10000, 10000, 10000]
+        self.max_point = [0, 0, 0]
 
     def now(self):
         return self.rtc.datetime
@@ -174,23 +241,15 @@ class Thermostat(object):
         self.last_stamp = now
 
     def run(self):
-        self.display.main()
+        self.gui.show_main()
         while True:
-            #self.stamp("start")
-            self.display.update_time(self.now())
-            #self.stamp("update_time")
+            self.gui.update_time(self.now())
             temperature_updates = self.mqtt.poll()
-            #self.stamp("mqtt poll")
             for (k, v) in temperature_updates:
                 self.temperatures[k] = v
-            #self.stamp("temp updates")
             if temperature_updates:
-                self.display.update_temperatures(self.temperatures)
-                #self.stamp("update_temperatures")
-
-            while not self.touch.buffer_empty:
-                print(self.touch.read_data())
-            #self.stamp("touch print")
+                self.gui.update_temperatures(self.temperatures)
+            self.gui.poll()
 
 def main():
     thermostat = Thermostat()
